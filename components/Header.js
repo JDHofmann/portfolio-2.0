c6a2c75069e5e487cc053bbe2254c2d0ef7043ee
jdhofmann <justindhofmann@gmail.com>
2021-04-08 17:03:14 -0400

    nav component props

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,10 @@ import Nav from './Nav';
 export default function Header(){
     return (
         <header>
-            <Nav classProp="main-menu"/>
+            <Nav 
+                classProp="main-menu"
+                ariaProp="main menu"
+            />
         </header>
     )
 }
--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,11 @@
 import Link from 'next/link';
 
-export default function Nav({classProp}){
+export default function Nav({classProp, ariaProp}){
     return(
-        <nav className={classProp}>
+        <nav 
+            className={classProp}
+            aria-label={ariaProp}
+        >
             <ul>
                 <li><Link href="/about">About</Link></li>
                 <li><Link href="/work">Work</Link></li>
