9fb7ba9eca2d579f94ff9bcb3b957b1de73d4734
jdhofmann <justindhofmann@gmail.com>
2021-04-08 12:34:25 -0400

    meta

--- components/Footer.js
+++ components/Footer.js
@@ -4,15 +4,6 @@ export default function Footer(){
     return (
         <footer>
             <Nav />
-            <style jsx>{`
-                nav {
-                    background: #00a000;
-                }
-                h1 {
-                    color: #ffffff;
-                    text-align: center;
-                }
-            `}</style>
         </footer>
     )
 }
--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,16 @@
 import Head from 'next/head'
 import Footer from './Footer'
 import Header from './Header'
+import { useRouter } from 'next/router'
 
 export default function Layout({children}){
+    const path = useRouter();
+    const pathname = path.pathname.slice(1);
     return(
         <div>
             <Head>
-                <title>JD Brewer-Hofmann</title>
-                <description>Portfolio Site for JD Brewer-Hofmann</description>
+                <title>JDBH {pathname}</title>
+                <meta name="description" content="Portfolio Site for JD Brewer-Hofmann"/>
             </Head>
             <Header />
             <main>{children}</main>
