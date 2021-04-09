bac057f036c589ef9938aa371c8c1fe4d8de9f83
jdhofmann <justindhofmann@gmail.com>
2021-04-09 13:59:07 -0400

    about page

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,11 @@ export default function About(){
     return (
         <Layout>
             <h1>About</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Orci a scelerisque purus semper eget duis at tellus. Cursus turpis massa tincidunt dui ut ornare. Nisl tincidunt eget nullam non nisi est sit amet facilisis. 
+                </p>
+                <p>
+                Cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Nisl pretium fusce id velit ut tortor pretium. At volutpat diam ut venenatis. Maecenas pharetra convallis posuere morbi. Venenatis cras sed felis eget velit aliquet sagittis. Maecenas ultricies mi eget mauris pharetra et. Aliquam ultrices sagittis orci a scelerisque purus. Porta lorem mollis aliquam ut porttitor leo a. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant. Tellus pellentesque eu tincidunt tortor aliquam nulla.</p>
         </Layout>
     )
 }
